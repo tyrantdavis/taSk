d26eb36c8d5a4df598cdcdbe77fe2935b004f7fc
Tyrant Davis <davistyrant@gmail.com>
2017-03-24 17:48:37 -0700

    Set host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # set :host to heroku here
+  config.action_mailer.default_url_options = { host: 'taskmetoo.herokuapp.com' }
 end
