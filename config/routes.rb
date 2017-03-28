61f5ffe1c9a7dc304fb67ffb027d1718d7d6e9a6
Tyrant Davis <davistyrant@gmail.com>
2017-03-28 10:34:43 -0700

    Redirect user to show view after sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
+  get 'users/show'
 
+  devise_for :users
+
+  authenticated :user do 
+        root 'users#show', as: :authenticated_root
+  end
+
+
+  get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
